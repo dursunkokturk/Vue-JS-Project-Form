fd2e76a24ad9fe2f11b630450540df516f617062
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-04-27 12:53:55 +0300

    print-data-entered-in-input-in-the-form-at-the-bottom-of-the-form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,23 +1,11 @@
 <template>
     <div>
         <h2>Form Projesi</h2>
-
-        <!--Form Uzerindeki Data lari Gonderme Isleminden Sonra
-            Sayfada Yenileme Islemini Durdurmak Icin
-            prevent Kullaniyoruz-->
         <form @submit.prevent="handleSubmit()">
             <label>Email:</label>
-
-            <!--Input Elementi Icine Girilen Degeri
-                v-model Property Icinde Yazilan Property ile
-                script Icinde Yer Alan Data Alani Icinde 
-                Ilgili Property e Gondererek
-                Yapilacak Islem Surecini Baslatiyoruz-->
             <input type="email" v-model="email" required>
             <label>Şifre:</label>
             <input type="password" v-model="password" required>
-
-            <!--Password Girilirken Hata Olursa Kullaniciya Gorunecek Satir-->
             <div class="error" v-if="passwordError">
                 {{passwordError}}
             </div>
@@ -27,29 +15,11 @@
                 <option value="female">Kadın</option>
             </select>
             <label>Bildiğiniz Diller</label>
-
-            <!--keyup Event Kullanilarak Input Element Icine Girilen Data yi
-                addSkill Metodu Uzerinden 
-                Property Olarak Kullanilan 
-                skills Array Icine Aliyoruz-->
             <input type="text" v-model="skill" @keyup.alt="addSkill($event)">
-
-            <!--skills Array Icinde Yer Alan Data lari Yazdirmak Icin
-                skills Array Icinde Tarama Yapiyoruz
-                Tarama Sonucunu skillItem Degiskeni Uzerinden Yazdiriyoruz-->
             <div v-for="skillItem in skills" :key="skillItem" class="skillItem">
-
-                <!--Ekarana Yazdirilan Data lari Uzerine Tiklandiginda
-                    Silinebilir Hale Getiriyoruz-->
                 <span @click="deleteSkill(skillItem)">{{skillItem}}</span>
             </div>
             <div class="term">
-
-                <!--Checkbox Elementi Icinde Degerin Secilme Durumunu
-                    v-model Property Icinde Yazilan Property ile
-                    script Icinde Yer Alan Data Alani Icinde 
-                    Ilgili Property e Gondererek
-                    Yapilacak Islem Surecini Baslatiyoruz-->
                 <input class="termInput" v-model="term" type="checkbox" required>
                 <label>Kullanım koşullarını kabul ediniz</label>
             </div>
@@ -83,14 +53,8 @@ export default {
     methods: {
         addSkill(event)
         {
-            // Input Icine Girilen Data yi Yazdirmak Icin
-            // , Karakterini Girmek Gerekiyor
             if(event.key === ',' && this.skill)
             {
-
-                // Girilen Data Daha Once Girildi Kontrol Ediyoruz
-                // Girildi Ise Yazdirma Islemini Yapmiyoruz
-                // Girilmedi Ise Yazdiriyoruz
                 if(!this.skills.includes(this.skill)){
                      this.skills.push(this.skill);
                 }               
@@ -98,21 +62,11 @@ export default {
             }      
         },
         deleteSkill(skill){
-
-            // Ekranda Yazili Olan Data larin Bulundugu Array Icinde Filtreleme Yapiyoruz
             this.skills=this.skills.filter(item =>{
-
-                // Uzerine Tiklanan Data Haricinde Data Var Ise
-                // O Data lari Donduruyoruz
                 return skill !== item
             })
         },
-
-        // Butona Tiklandiginda Gonderme Islemi Icin
-        // Form Uzerinden Alinacak Data lar
         handleSubmit(){
-
-            // Password Girilirken Validation Islemi Yapimi
             this.passwordError = this.password.length > 5 ? '' : 'Lütfen minumum 6 karakterli bir şifre giriniz'
             console.log('Email',this.email)
             console.log('Passsword',this.password)
